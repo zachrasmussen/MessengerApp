03f2b72371ef4556965d111e56576d65fbfe7f2b
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-25 16:41:24 -0600

    added customTextField file to Utils

--- MessengerApp/View/Authentication/LoginView.swift
+++ MessengerApp/View/Authentication/LoginView.swift
@@ -101,30 +101,3 @@ struct LoginView_Previews: PreviewProvider {
     }
 }
 
-struct CustomTextField: View {
-    let imageName: String
-    let placeholderText: String
-    let isSecureField: Bool
-    @Binding var text: String
-    
-    var body: some View {
-        VStack(spacing: 16) {
-            HStack {
-                Image(systemName: imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 20, height: 20)
-                    .foregroundColor(Color(.darkGray))
-
-                if isSecureField {
-                    SecureField(placeholderText, text: $text)
-                } else {
-                    TextField(placeholderText, text: $text)
-                }
-            }
-            
-            Divider()
-                .background(Color(.darkGray))
-        }
-    }
-}
