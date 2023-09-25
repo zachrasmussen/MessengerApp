5d61c9afe1318b99cef4d59634ff07c7fa60f746
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-25 13:47:11 -0600

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Zachary Rasmussen on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
