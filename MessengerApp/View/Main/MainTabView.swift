9e1aff5040012cb2087886294e7634e338e6fe17
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-28 14:22:40 -0600

    created mainTabView for navigation on the bottom

---
+++ MessengerApp/View/Main/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  MessengerApp
+//
+//  Created by Zachary Rasmussen on 9/28/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
