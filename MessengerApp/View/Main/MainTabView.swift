6677173decb818e3abfc8d8bbfffce77e41d4d49
Zach Rasmussen <zsrasmussen@gmail.com>
2023-09-28 14:45:56 -0600

    added tab title to each page

--- MessengerApp/View/Main/MainTabView.swift
+++ MessengerApp/View/Main/MainTabView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            TabView(selection: $selectedIndex) {
+                ConversationsView()
+                    .onTapGesture {
+                         selectedIndex = 0
+                    }
+                    .tabItem { Image(systemName: "bubble.left") }
+                    .tag(0)
+                
+                ChannelsView()
+                    .onTapGesture {
+                         selectedIndex = 1
+                    }
+                    .tabItem { Image(systemName: "bubble.left.and.bubble.right") }
+                    .tag(1)
+                
+                SettingsView()
+                    .onTapGesture {
+                         selectedIndex = 2
+                    }
+                    .tabItem { Image(systemName: "gear") }
+                    .tag(2)
+            }
+            .navigationTitle(tabTitle)
+        }
+    }
+    
+    var tabTitle: String {
+        switch selectedIndex {
+        case 0: return "Chats"
+        case 1: return "Channels"
+        case 2: return "Settings"
+        default: return ""
+        }
     }
 }
 
