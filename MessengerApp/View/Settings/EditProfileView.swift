9f71c83ff5719dad16c2bd537adceecffd4e52ed
Zach Rasmussen <zsrasmussen@gmail.com>
2023-10-03 14:42:40 -0600

    editProfileView

---
+++ MessengerApp/View/Settings/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  MessengerApp
+//
+//  Created by Zachary Rasmussen on 10/3/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
