02879b11e292626d33d4f3e4a09f8feaba884f15
Zach Rasmussen <zsrasmussen@gmail.com>
2023-10-03 15:01:16 -0600

    status selector

---
+++ MessengerApp/View/Settings/StatusSelectorView.swift
@@ -0,0 +1,18 @@
+//
+//  StatusSelectorView.swift
+//  MessengerApp
+//
+//  Created by Zachary Rasmussen on 10/3/23.
+//
+
+import SwiftUI
+
+struct StatusSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusSelectorView()
+}
