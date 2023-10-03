b2c46aeec04d0bf2ffcbb23216b6108f7eb95bb4
Zach Rasmussen <zsrasmussen@gmail.com>
2023-10-03 09:11:37 -0600

    created SettingsHeaderView, SettingsCell

---
+++ MessengerApp/View/Settings/SettingsCell.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsCell.swift
+//  MessengerApp
+//
+//  Created by Zachary Rasmussen on 10/3/23.
+//
+
+import SwiftUI
+
+struct SettingsCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsCell()
+}
