94a73e38c1be9d27c1a37ec317cf9e3bdf19607e
Zach Rasmussen <zsrasmussen@gmail.com>
2023-10-03 15:23:58 -0600

    StatusViewModel enum

---
+++ MessengerApp/ViewModel/StatusViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  StatusViewModel.swift
+//  MessengerApp
+//
+//  Created by Zachary Rasmussen on 10/3/23.
+//
+
+import SwiftUI
+
+struct StatusViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusViewModel()
+}
